00b80860382cbd04b3dbf0599abfe3aa1cc742bd
ramonkroetz <ramon.kroetz@cwi.com.br>
2021-05-28 13:57:47 -0300

    webpack with babel

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+    entry: path.resolve(__dirname, './index.js'),
+    output: {
+      path: path.resolve(__dirname, './lib'),
+      filename: 'index.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js']
+    },
+}
